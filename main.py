7b7ffb393084398b8fde574f7977b88d59160b77
diboooon <134046071+diboooon@users.noreply.github.com>
2023-12-22 21:12:39 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from prob_calculator import Hat, experiment
+
+# Example usage
+hat = Hat(black=6, red=4, green=3)
+probability = experiment(hat=hat, expected_balls={"red": 2, "green": 1}, num_balls_drawn=5, num_experiments=2000)
+
+print(f"Probability: {probability}")
